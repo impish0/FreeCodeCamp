bc60d3426eff5089a7e6060038f48c9c34c6bceb
impish0 <68518906+impish0@users.noreply.github.com>
2025-11-29 13:52:35 -0900

    Remove outdated theory files and add comprehensive JavaScript data types and string manipulation lessons

---
+++ Javascript/Variables_and_Strings/WorkShop/TriviaBot/javascriptTriviaBot.js
@@ -0,0 +1,22 @@
+console.log("Hello! I'm your coding fun fact guide!");
+
+let botName = "Rufus";
+let botLocation = "Jupitor";
+let favoriteLanguage = "Javascript";
+
+console.log("My name is " + botName + " and I live on " + botLocation + ".");
+console.log("My favorite programming language is " + favoriteLanguage + ".");
+
+let codingFact = favoriteLanguage + " was created in just 10 days!";
+
+console.log(codingFact);
+
+codingFact = "Also " + favoriteLanguage + " was made by one guy";
+
+console.log(codingFact);
+
+codingFact = favoriteLanguage + " runs in every browser!"
+
+console.log(codingFact);
+
+console.log("It was fun sharing these facts with you. Goodbye!" + " - " + botName + " from " + botLocation + ".");
