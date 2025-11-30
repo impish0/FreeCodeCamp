4e1dc8af4bda6d17cef11d40f4b115594c05d52c
impish0 <68518906+impish0@users.noreply.github.com>
2025-11-29 16:58:57 -0900

    Added sentence maker project js, and re-names dirs for consistancy

---
+++ Javascript/Variables_and_Strings/Lab/sentanceMaker/script.js
@@ -0,0 +1,25 @@
+// assign initial variables
+let adjective = "slow";
+let noun = "pig";
+let verb = "hot";
+let place = "Alaska";
+let adjective2 = "large";
+let noun2 = "pizza";
+
+// Setup the first story
+const firstStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + "." + " The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+
+console.log("First story: " + firstStory);
+
+// reassign the variables
+adjective = "fast";
+noun = "horse";
+verb = "cold";
+place = "Texas";
+adjective2 = "tiny";
+noun2 = "tacos";
+
+// assemble the second story
+const secondStory = "Once upon a time, there was a(n) " + adjective + " " +noun + " who loved to eat " + noun2 + ". " + "The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+
+console.log("Second story: " + secondStory);
